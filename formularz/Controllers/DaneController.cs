b26cee84d303fc9c135900c70fd8c9c225b33eb9
grzegorz.moskala <grzegorz.moskala@HK-19>
2023-10-29 13:29:23 +0100

    Add project files.

---
+++ formularz/Controllers/DaneController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using formularz.Models;
+
+namespace formularz.Controllers
+{
+    public class DaneController : Controller
+    {
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", dane);
+            }
+            else return View();
+        }
+        public IActionResult Wynik(Dane dane)
+        {
+            return View(dane);
+        }
+    }
+}
---
+++ formularz/Models/Dane.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace formularz.Models
+{
+    public class Dane
+    {
+     
+        [Required(ErrorMessage = "Prosze podaj Imie")]
+        [MinLength(2)]
+        public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Nazwisko")]
+        [MinLength(2)]
+        public string Nazwisko { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Podaj Hasło")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Podaj potwierdzenie hasła")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirmation { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set;}
+
+        [Range(10,80)]
+        public int Wiek { get; set; }
+
+
+
+    }
+}
